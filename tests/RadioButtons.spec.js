6119fa44903b8e23abd5554c615fe66ee5878f2a
MaheshwariVemula <maheshwarivemula@hotmail.com>
2024-11-23 00:38:23 +0100

    Male RadioButton check

---
+++ tests/RadioButtons.spec.js
@@ -0,0 +1,18 @@
+const { test,expect } = require("@playwright/test");
+
+test('radio buttons',async({page})=>{
+    await page.goto('https://testautomationpractice.blogspot.com/');
+
+    //Radio Button
+    await page.locator("//input[@id='male']").check(); //male
+    // await page.check("//input[@id='male']");
+    await expect(await page.locator("//input[@id='male']")).toBeChecked();
+    await expect(await page.locator("//input[@id='male']").isChecked()).toBeTruthy();
+
+
+
+
+    await page.waitForTimeout(5000) //Pausing Code
+
+})
+
