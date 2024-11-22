d82e4a4b3cd034a155c1dc0fa8ca69c3734b1ce7
MaheshwariVemula <maheshwarivemula@hotmail.com>
2024-11-22 14:14:19 +0100

    Again Assertions

--- tests/Assertions.spec.js
+++ tests/Assertions.spec.js
@@ -11,36 +11,34 @@ test("AssertionsTest", async ({ page }) => {
   await expect(page).toHaveTitle("nopCommerce demo store. Register");
 
   //3) expect(locator).toBeVisible() Element is visible
-const logoElement=await page.locator('.header-logo')
-await expect(logoElement).toBeVisible()
+  const logoElement = await page.locator(".header-logo");
+  await expect(logoElement).toBeVisible();
 
+  //4) expect(locator).toBeEnabled()  Control is enabled
+  const searchBox = await page.locator("#small-searchterms");
+  await expect(searchBox).toBeEnabled();
 
-//4) expect(locator).toBeEnabled()  Control is enabled
-const searchBox=await page.locator('#small-searchterms')
-await expect(searchBox).toBeEnabled()
+  //5) await expect(locator).toBeChecked()  Checkbox is checked
+  const radioButton = await page.locator("#gender-male");
+  await radioButton.click(); // select Radio Button
+  await expect(radioButton).toBeChecked();
 
-//5) await expect(locator).toBeChecked()  Checkbox is checked
-const radioButton = await page.locator('#gender-male')
-await radioButton.click() // select Radio Button
-await expect(radioButton).toBeChecked()
+  //check box
+  const newsLetter = await page.locator("#Newsletter");
+  await expect(newsLetter).toBeChecked();
 
-//check box
-const newsLetter = await page.locator('#Newsletter')
-await expect(newsLetter).toBeChecked()
+  //6) await expect(locator).toHaveAttribute()    Element has a DOM attribute
 
-//6) await expect(locator).toHaveAttribute()    Element has a DOM attribute
+  const registerButton = await page.locator("#register-button");
+  await expect(registerButton).toHaveAttribute("type", "submit");
 
-const registerButton = await page.locator('#register-button')
-await expect(registerButton).toHaveAttribute('type', 'submit')
+  //7) await expect(locator).toHaveText()    Element matches text
 
-//7) await expect(locator).toHaveText()    Element matches text
+  await expect(await page.locator(".page-title h1")).toHaveText("Register"); //Full Text
 
-await expect(await page.locator('.page-title h1')).toHaveText('Register') //Full Text
+  //8) await expect(locator).toContainText()  Element contains text
 
-//8) await expect(locator).toContainText()  Element contains text
+  await expect(await page.locator(".page-title h1")).toContainText("Reg"); //Partial Text
 
-await expect(await page.locator('.page-title h1')).toContainText('Reg') //Partial Text
+  
 });
-
-
-
