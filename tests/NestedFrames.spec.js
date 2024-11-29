e638e81fd9bbf13a5b7a50b3471f6b3d334e3ed5
MaheshwariVemula <maheshwarivemula@hotmail.com>
2024-11-29 11:50:20 +0100

    Nested Frames

---
+++ tests/NestedFrames.spec.js
@@ -0,0 +1,12 @@
+const{test,expect}=require('@playwright/test')
+test('Nested Frames',async({page})=>{
+    await page.goto('https://ui.vision/demo/webtest/frames/')
+    const frame3=await page.frame({url:'https://ui.vision/demo/webtest/frames/frame_3.html'})
+    //await frame3.locator("input[name='mytext3']").fill("welcome")
+
+    //Nested Frame
+    const childFrames=await frame3.childFrames()
+ await childFrames[0].locator("//*[@id='i6']/div[3]/div").check()
+
+    await page.waitForTimeout(5000);
+})
