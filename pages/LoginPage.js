e553e8d36e3b43faa172b5d6d2367bd65266df14
MaheshwariVemula <maheshwarivemula@hotmail.com>
2024-12-31 02:37:01 +0100

    POM method

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,1 +1,20 @@
+exports.LoginPage = class LoginPage {
+  constructor(page) {
+    this.page = page;
+    this.LoginLink = "#login2";
+    this.usernameInput = "#loginusername";
+    this.passwordInput = "#loginpassword";
+    this.loginButton = "//button[normalize-space()='Log in']";
+  }
+  async gotoLoginPage(){
+    await this.page.goto("https://www.demoblaze.com/index.html")
+  }
+  async login(username,password){
+    await this.page.locator(this.LoginLink).click()
+    await this.page.locator(this.usernameInput).fill(username)
+    await this.page.locator(this.passwordInput).fill(password)
+    await this.page.locator(this.loginButton).click()
+
+  }
+  }
 
--- tests/POMtest.spec.js
+++ tests/POMtest.spec.js
@@ -1,4 +1,21 @@
 
+//const{test,expect}= require('@playwright/test')
+import { test, expect } from '@playwright/test'
+import { LoginPage } from '../pages/LoginPage'
+test('test',async({page})=>{
+    //Log in
+    const login = new LoginPage(page)
+    await login.gotoLoginPage()
+    await login.login('pavanol','test@123')
+    await page.waitForTimeout(5000)
 
+
+
+    //Home Page
+
+
+    //Cart
+
+})
 
 
